ce43020748252ccf226ceff60e36ebe14f9a83f7
niekvanrabenswaaij <niekvrabenswaaij@gmail.com>
2022-06-17 11:04:19 +0200

    0.1.0-Beta-1.3 - update onboarding steps

--- src/app/tour-steps.ts
+++ src/app/tour-steps.ts
@@ -25,134 +25,143 @@ const builtInButtons = {
 const tourStepsTextAndTitle = [
   {
     id: 'step_1',
-    title: 'Welcome (1/23)',
-    text: 'The next steps will guide you through this application and shows you how the application works',
+    title: 'Welcome (1/24)',
+    text: 'The next steps will guide you through this application and shows you how the application works.',
   },
   {
     id: 'step_2',
-    title: 'Applicants (2/23)',
-    text: 'This is the list of this years applicants. It\' scrollable and, at this moment, sorted by entry id'
+    title: 'Applicants (2/24)',
+    text: 'This is the list of this years applicants. It\'s scrollable and, at this moment, sorted by application number.'
   },
   {
     id: 'step_3',
-    title: 'Applicants (3/23)',
-    text: 'Using this button you can also sort on applicant status. An applicant can have the following statuses: ' +
-      'Submitted and Skipped. Selecting this button sorts the applicants showing applicants without a status first, ' +
-      'then applicants with a Skipped status and lastly applicants with a Submitted status. Selecting this button ' +
-      'again reverts the status order (submitted, skipped, none) and a third selection orders the applicant on entry id (ascending)'
+    title: 'Applicants (3/24)',
+    text: 'With this button you can sort the applicants differently, based on their status. An applicant can have the ' +
+      'following statuses: None, Skipped, Submitted. Clicking this button once sorts the applicants in this order: ' +
+      'None, Skipped, Submitted. Clicking again sorts them in the order Submitted, Skipped, None. Clicking a third ' +
+      'time sorts on the application number again (ascending).'
   },
   {
     id: 'step_4',
-    title: 'Applicants (4/23)',
-    text: 'Using this field you are able to search for specific applicants. It is possible to search on entry id or any part of the applicants name'
+    title: 'Applicants (4/24)',
+    text: 'Using this field you are able to search for specific applicants. It is possible to search on application number or any part of the applicants name.'
   },
   {
     id: 'step_5',
-    title: 'Applicants (5/23)',
-    text: 'You can select any applicant in the list to open his/her application. <br><br><b>Now select this application</b>',
+    title: 'Applicants (5/24)',
+    text: 'You can select any applicant in the list to open his/her application. <br><br><b>Now select this application</b>.',
   },
   {
     id: 'step_6',
-    title: 'Applicants (6/23)',
-    text: 'You can see that this applicant is now selected'
+    title: 'Applicants (6/24)',
+    text: 'You can see that this applicant is now selected, as it is highlighted in black and green.'
   },
   {
     id: 'step_7',
-    title: 'Application (7/23)',
-    text: 'You can see the selected applicant and it\'s entry id over her as well'
+    title: 'Application (7/24)',
+    text: 'The selected applicant’s number and name will appear on top  of the screen as well (on the right).'
   },
   {
     id: 'step_8',
-    title: 'Application (8/23)',
-    text: 'At the mid-part of the application you can find anything that is known about this application'
+    title: 'Application (8/24)',
+    text: 'In the centre of the screen, the application form of the applicant appears.'
   },
   {
     id: 'step_9',
-    title: 'Scoring (9/23)',
-    text: 'At the right side you are able to score the application'
+    title: 'Scoring (9/24)',
+    text: 'On the right, the scoring panel is visible.'
   },
   {
     id: 'step_10',
-    title: 'Scoring (10/23)',
-    text: 'Use these fields to score the application on the given criteria. Score between a 1 and a 5 (1 being lowest, 5 highest)'
+    title: 'Scoring (10/24)',
+    text: 'Use these fields to score the application on the given criteria. Score between a 1.0 and a 5.0 (1.0 being lowest, 5.0 highest).'
   },
   {
     id: 'step_11',
-    title: 'Scoring (11/23)',
-    text: 'Click on the \'i\' to open the category description if needed'
+    title: 'Scoring (11/24)',
+    text: 'Click on the \'i\' to open the category description if needed.'
   },
   {
     id: 'step_12',
-    title: 'Scoring (12/23)',
-    text: 'Every category as a subtotal score'
+    title: 'Scoring (12/24)',
+    text: 'Subtotals for each category will automatically be calculated.'
   },
   {
     id: 'step_13',
-    title: 'Scoring (13/23)',
-    text: 'All scores add up to an average total score'
+    title: 'Scoring (13/24)',
+    text: 'As well as the total score.'
   },
   {
     id: 'step_14',
-    title: 'Scoring (14/23)',
-    text: 'You can use this button to skip scoring this application. Skipping means you are not ready to completely ' +
-      'score this application and want to take a better look into it later. Skipping als means that the applicant in ' +
-      'the applicant-list (left-panel) gets a grey bar in front of its name and entry id, reminding you that you ' +
-      'still have some scoring to do for that applicant'
+    title: 'Scoring (14/24)',
+    text: 'You can use this button to skip scoring this applicant. Skipping an applicant means that you find this an ' +
+      'unworthy application, and you will not spend time scoring it in depth. The applicant will get the status ' +
+      '‘Skipped’ in the list on the left panel (visual by a grey bar). You can always return to the applicant and ' +
+      'still score it if you change your mind.'
   },
   {
     id: 'step_15',
-    title: 'Scoring (15/23)',
-    text: 'When all scores are given you are able to submit these scores. Submitting freezes the scores, but you are ' +
-      'able to change the scores. When you submit the scores these buttons will disappear and an \'edit\'-button ' +
-      'appears. Select the edit button to reopen the scores. This does require you to resubmit the scores again when you are done'
+    title: 'Scoring (15/24)',
+    text: 'When all categories are scored (you cannot leave the 0), you are able to click ‘Submit’ here. Submitting ' +
+      'freezes the scores for this applicant. An ‘Edit’ button will appear which you can use to alter the scores if ' +
+      'you change your mind afterwards. Editing does require you to resubmit the scores when you are done.'
   },
   {
     id: 'step_16',
-    title: 'Scoring (16/23)',
-    text: 'Another way to continue is to leave all scores blank. When an application is not good enough to consider ' +
-      'it might be easier to just leave all scores blank. When all scores are blank it also counts as a submitted ' +
-      'application. When you started scoring, but want to create a blank scoresheet again, just select this button. ' +
-      'It will reset all your scores'
+    title: 'Scoring (16/24)',
+    text: 'This button resets all scores for the selected applicant back to 0. You can use it to start over. Also, if ' +
+      'you are scoring an applicant but at some point feel that it is not worth the time to continue scoring it, ' +
+      'you should click ‘Reset scores’ as well, before being able to Skip below.'
   },
   {
     id: 'step_17',
-    title: 'Scoring (17/23)',
-    text: 'In this field you can write comments for this application'
+    title: 'Scoring (17/24)',
+    text: 'In this field you can write comments for this application.'
   },
   {
     id: 'step_18',
-    title: 'General (18/23)',
-    text: 'Select this button to log-out'
+    title: 'General (18/24)',
+    text: 'If you leave an applicant halfway through scoring, your scores and comments will be saved automatically for ' +
+      'you to continue when you get back to it.'
   },
   {
     id: 'step_19',
-    title: 'General (19/23)',
-    text: 'Select this button to start this application introduction again'
+    title: 'General (19/24)',
+    text: 'Select this <i class="fas fa-power-off"></i> button in the left bottom corner to log-out of the scoring webtool.'
   },
   {
     id: 'step_20',
-    title: 'Overall score (20/23)',
-    text: 'Select this button to open an overview of the scores of all applicants. <br><br><b>Now click this button to open this overview</b>',
+    title: 'General (20/24)',
+    text: 'Select this <i class="fas fa-question"> button to start the guidance through the webtool again any time while using the tool.'
   },
   {
     id: 'step_21',
-    title: 'Overall score (21/23)',
+    title: 'Overall score (21/24)',
+    text: 'By clicking on the ‘Overall scores’ button in the left bottom of the screen, you can open an overview of the ' +
+      ' scores for all the applications you have submitted scores for so far. It will open in a different window. ' +
+      '<br><br><b>Now click this button to open this overview</b>',
+  },
+  {
+    id: 'step_22',
+    title: 'Overall score (22/24)',
     text: 'In this overview you can see all scores you have given for the different applicants. The applicants are ' +
       'sorted by total score, highest score on top'
   },
   {
-    id: 'step_22',
-    title: 'Overall score (22/23)',
-    text: 'When you have judged all applications (so if all applications are either not scored at all or submitted) ' +
-      'you will be able to use this button to submit all your scores. Be carefull though, after submitting the scores ' +
-      'can not be changed again. This submissions is final'
+    id: 'step_23',
+    title: 'Overall score (23/24)',
+    text: 'When you have judged all applications (so all applications are either Submitted or Skipped) you will be ' +
+      'able to use this button to submit your final scores to the Future For Nature office. After submitting, all your' +
+      'scores will be frozen and you cannot adjust them anymore. You will still be able to log-in and read the ' +
+      'applications and your comments.'
   },
   {
 
-    id: 'step_23',
-    title: 'Overall score (23/23)',
-    text: 'This concludes the tour of the application. If you want to see it again, press the \'question mark\'-button ' +
-      'in the bottom-left corner of the screen. If you still have questions, don\'t hesitate to contact future for nature!'
+    id: 'step_24',
+    title: 'Overall score (24/24)',
+    text: 'This concludes the tour of the web-tool. If you want to go through it again, click the ‘question-mark’-button ' +
+      'in the bottom-left corner of the screen. If you have any further questions, don’t hesitate to contact the ' +
+      'Future For Nature team.'
   }
 ]
 
@@ -435,6 +444,18 @@ export const TOUR_STEPS = [
       element: '.left-panel button.small.border',
       on: 'right'
     },
+    buttons: [
+      {
+        ...builtInButtons.back,
+        secondary: true
+      },
+      builtInButtons.next
+    ],
+    scrollTo: false,
+  },
+  {
+    ...tourStepsTextAndTitle[ 17 ],
+    arrow: false,
     buttons: [
       {
         ...builtInButtons.back,
