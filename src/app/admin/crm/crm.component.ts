7ce44128dc791c7cd4d0cdb19f628c089c1fd534
niekvanrabenswaaij <niekvrabenswaaij@gmail.com>
2024-01-18 14:49:33 +0100

    1.1.3-CRM-0.8 - add data limiter in dev mode

--- src/app/admin/crm/crm.component.ts
+++ src/app/admin/crm/crm.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, isDevMode, OnInit, ViewChild } from '@angular/core';
 import { from, map, Observable, switchMap, take, tap } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
@@ -125,8 +125,8 @@ export class CrmComponent implements OnInit {
               private location: Location) { }
 
   ngOnInit(): void {
-    this.relationsCollectionService.getLimitNumberOfItems(50)
-      .pipe(
+    const observable = isDevMode() ? this.relationsCollectionService.getLimitNumberOfItems(50) : this.relationsCollectionService.getAll();
+    observable.pipe(
         take(1),
         tap(relations => {
           this.relationData = relations;
@@ -134,7 +134,9 @@ export class CrmComponent implements OnInit {
         }),
         switchMap(() => this.eventCollectionService.getAll().pipe(take(1))),
         tap(events => this.events = events),
-        switchMap(() => this.registrationCollectionService.getLimitNumberOfItems(50).pipe(take(1))),
+        switchMap(() => isDevMode() ?
+          this.registrationCollectionService.getAll().pipe(take(1)) :
+          this.registrationCollectionService.getLimitNumberOfItems(50).pipe(take(1))),
         tap(registrations => {
           this.registrationData = registrations.map(registration => this.mapInviteToRegistration(registration));
           this.registrationData.sort((a, b) => this.sortOnRelationCodeAndEvent(a, b));
